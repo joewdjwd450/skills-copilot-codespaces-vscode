c29e0d1497ad9ba006ae142472acbc619b2ee8a0
joewdjwd450 <64016773+joewdjwd450@users.noreply.github.com>
2023-07-23 14:42:48 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,14 @@
+// Purpose: To calculate the sum of two numbers
+function calculateNumbers(var1, var2)
+
+{
+    var sum = var1 + var2;
+    return sum;
+}
+
+function calculateNumbers2(var1, var2)
+
+{
+    var sum = var1 - var2;
+    return sum;
+}
