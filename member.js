09f1a9422296f4d84f202faf5fd307ec7fa1f673
joewdjwd450 <64016773+joewdjwd450@users.noreply.github.com>
2023-07-23 14:49:40 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,25 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberContent = document.getElementById("memberContent");
+    var skillsContent = document.getElementById("skillsContent");
+    var projectsContent = document.getElementById("projectsContent");
+    var contactContent = document.getElementById("contactContent");
+    var aboutContent = document.getElementById("aboutContent");
+
+    member.style.color = "#00bfff";
+    skills.style.color = "#fff";
+    projects.style.color = "#fff";
+    contact.style.color = "#fff";
+    about.style.color = "#fff";
+
+    memberContent.style.display = "block";
+    skillsContent.style.display = "none";
+    projectsContent.style.display = "none";
+    contactContent.style.display = "none";
+    aboutContent.style.display = "none";
+}
+  
